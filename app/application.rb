fa5c48f544b59b80ac5bdf4982b164d69f1f1590
Madeline Stark <madeline.stark1@gmail.com>
2018-08-23 18:00:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,16 +2,13 @@ require 'pry'
 
 class Application
 
-#@@item = []
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
      if req.path.match(/items/)
        item_name = req.path.split("/items/").last
-       binding.pry
-        if item = @@item.find{|i| i.name == item_name}
+        if item = @@items.find{|i| i.name == item_name}
             resp.write "#{item.price}"
         else
             resp.write "Item not found"
