8afd2432420bf6b444b19a602daaaaea824ef98e
Madeline Stark <madeline.stark1@gmail.com>
2018-08-23 17:58:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
 
      if req.path.match(/items/)
        item_name = req.path.split("/items/").last
+       binding.pry
         if item = @@item.find{|i| i.name == item_name}
             resp.write "#{item.price}"
         else
